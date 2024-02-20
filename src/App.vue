3daee44050c7fcccf049c671309a8822d3914804
Moazam05 <salmanmoazam08@gmail.com>
2024-02-20 14:49:26 +0500

    first commit

---
+++ src/App.vue
@@ -0,0 +1,10 @@
+<script setup>
+import HelloWorld from "./components/HelloWorld.vue";
+import Home from "./components/Home.vue";
+</script>
+
+<template>
+  <div></div>
+  <!-- <HelloWorld msg="Vite + Vue" name="Salman" /> -->
+  <Home />
+</template>
