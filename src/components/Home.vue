c71c22bbb1eeb4d0bfd2838a270ed488235fb1cb
Moazam05 <salmanmoazam08@gmail.com>
2024-02-20 15:05:54 +0500

    send data from script to html

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,9 +1,38 @@
 <template>
-  <div>Hi</div>
+  <div
+    :style="{
+      color: 'red',
+      fontSize: '20px',
+      fontWeight: 'bold',
+    }"
+  >
+    Home Page
+  </div>
+  <div>Email: {{ email }}</div>
+  <div>Phone: {{ phone }}</div>
+  <div>Name: {{ getName("Salman") }}</div>
+  <div>Name: {{ getName("Kashif") }}</div>
+  <!-- <div>HotMail: {{ hotMail }}</div> -->
 </template>
 
 <script>
 export default {
   name: "Home",
+
+  setup() {
+    const email = "salmanmoazam08@gmail.com";
+    const phone = "1234567890";
+    const getName = (name) => {
+      return name;
+    };
+
+    return { email, phone, getName };
+  },
+
+  // data() {
+  //   return {
+  //     hotMail: "salman@hotmail.com",
+  //   };
+  // },
 };
 </script>
